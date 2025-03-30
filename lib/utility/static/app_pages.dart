61e5a9af487e3b06ec368e98e0f60c6ac2ebd7c0
JH <jhk3261@gnu.ac.kr>
2025-03-30 21:04:47 +0900

    ✨ feat : init Setting(routes,pages)

---
+++ lib/utility/static/app_pages.dart
@@ -0,0 +1,10 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'app_routes.dart';
+
+List<GetPage> appPages = [
+  // TODO(ALL) : (홈/복습/문제집/마이페이지 메인 화면 제외) 페이지 추가하기 전에 GetPage 설정하기
+  // GetPage(
+  //   name: Routes.LOGIN,
+  //   page: () => const LoginScreen(),
+  // ),
+];
