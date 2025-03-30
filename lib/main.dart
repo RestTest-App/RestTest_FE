bb67ff081b6105cf2d84cdb154718fdd87a9ab36
JH <jhk3261@gnu.ac.kr>
2025-03-31 01:01:36 +0900

    ✨feat : init BoilerPlate & asset Setting

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ Future<void> onSystemInit() async{
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   // Environment
-  await dotenv.load(fileName: "assets/config/.env");
-  // KakaoSdk.init(nativeAppKey: "${dotenv.env['KAKAO_APP_KEY']}");
+
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // 스플래시 스크린 제거
