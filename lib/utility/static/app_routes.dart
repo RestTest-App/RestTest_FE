6a20d341458d93d987ca82a8c14c906acdbf06ac
JH <jhk3261@gnu.ac.kr>
2025-03-30 21:04:47 +0900

    ✨ feat : init Setting(routes,pages)

---
+++ lib/utility/static/app_routes.dart
@@ -0,0 +1,9 @@
+abstract class Routes {
+  static const String SPLASH = "/splash";
+  static const String ON_BOARDING = "/on_boarding";
+  static const String LOGIN = "/login";
+  static const String ROOT = "/";
+  static const String TEST = "/test/:testId";
+  static const String REVIEW_ITEM = "/review/:reviewId";
+  static const String UPDATE_NICKNAME = "/update_nickname";
+}
