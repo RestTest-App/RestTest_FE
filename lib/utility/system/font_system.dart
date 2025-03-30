86cf6fb6d1fca0145205829ce16ec220601d8e76
JH <jhk3261@gnu.ac.kr>
2025-03-30 20:43:43 +0900

    ✨ feat : init System

---
+++ lib/utility/system/font_system.dart
@@ -0,0 +1,309 @@
+import 'package:flutter/material.dart';
+
+abstract class FontSystem {
+  /* ----------------------- 28 ----------------------- */
+  static const TextStyle KR28EB = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.5,
+  );
+  static const TextStyle KR28B = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.5,
+  );
+  /* ----------------------- 24 ----------------------- */
+  static const TextStyle KR24EB = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR24B = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR24SB = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  /* ----------------------- 22 ----------------------- */
+  static const TextStyle KR22EB = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.3,
+  );
+  static const TextStyle KR22B = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.3,
+  );
+  static const TextStyle KR22SB = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.3,
+  );
+  static const TextStyle KR22M = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.3,
+  );
+  /* ----------------------- 20 ----------------------- */
+  static const TextStyle KR20EB = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.2,
+  );
+  static const TextStyle KR20B = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.2,
+  );
+  static const TextStyle KR20SB = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.2,
+  );
+  static const TextStyle KR20M = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.2,
+  );
+  /* ----------------------- 18 ----------------------- */
+  static const TextStyle KR18B = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR18SB = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR18M = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR18R = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  /* ----------------------- 16 ----------------------- */
+  static const TextStyle KR16B = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR16SB = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR16M = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR16R = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR16L = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  /* ----------------------- 14 ----------------------- */
+  static const TextStyle KR14B = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR14SB = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR14M = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR14R = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR14L = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  /* ----------------------- 12 ----------------------- */
+  static const TextStyle KR12B = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR12SB = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR12M = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR12R = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR12L = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  /* ----------------------- 10 ----------------------- */
+  static const TextStyle KR10B = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR10SB = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR10M = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR10R = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+  static const TextStyle KR10L = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    fontFamily: 'AppleSDGothicNeo',
+    color: Color(0xFF181818),
+    letterSpacing: -0.6,
+    height : 1.0,
+  );
+}
