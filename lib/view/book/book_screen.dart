6e1f2cc25fcdcc9a8f9e0f5e71d90567aa36b481
JongHwa Paik <qorwhdghk12@gmail.com>
2025-04-07 21:05:28 +0900

    ✨ feat : base design

--- lib/view/book/book_screen.dart
+++ lib/view/book/book_screen.dart
@@ -1,16 +1,11 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rest_test/utility/system/color_system.dart';
 import 'package:rest_test/view/base/base_screen.dart';
 import 'package:rest_test/viewmodel/book/book_view_model.dart';
-import 'package:rest_test/viewmodel/home/home_view_model.dart';
-import 'package:rest_test/widget/appbar/default_back_appbar.dart';
+import 'package:dotted_border/dotted_border.dart';
 
-import '../../widget/appbar/default_appbar.dart';
-
-class BookScreen extends BaseScreen<BookViewModel>{
+class BookScreen extends BaseScreen<BookViewModel> {
   const BookScreen({super.key});
 
   @override
@@ -28,21 +23,310 @@ class BookScreen extends BaseScreen<BookViewModel>{
   @override
   PreferredSizeWidget? buildAppBar(BuildContext context) {
     return PreferredSize(
-        preferredSize: const Size.fromHeight(58),
-        child: DefaultAppBar(
-          title: '나의 문제집',
-          backColor : ColorSystem.back,
-        ));
+      preferredSize: const Size.fromHeight(58),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+        child: AppBar(
+          title: Center(
+            child: Text(
+              '나의 문제집',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'AppleSDGothicNeo',
+                fontWeight: FontWeight.w600, // SemiBold
+              ),
+            ),
+          ),
+          backgroundColor: ColorSystem.back,
+          elevation: 0,
+        ),
+      ),
+    );
   }
 
   @override
   Widget buildBody(BuildContext context) {
-    // return Obx((){
-      return const Column(
+    // 드롭다운 아이템 리스트
+    List<String> dropdownItems = ['최근 생성순', '가나다순', '최근 학습순'];
+    String selectedValue = dropdownItems[0]; // 초기 선택 값
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
         children: [
-          Text("book")
+          // 배너 광고 영역
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            height: 150.0,
+            color: Colors.grey[300],
+            child: Center(
+              child: Text(
+                '배너 광고 영역',
+                style: TextStyle(color: Colors.grey[700]),
+              ),
+            ),
+          ),
+          // 드롭박스와 total 4 텍스트를 양쪽 끝에 배치
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 23.0), // 위아래 패딩 23pt
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // 왼쪽 드롭박스
+                Container(
+                  height: 24,
+                  width: 110,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: selectedValue,
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        fontFamily: 'AppleSDGothicNeo',
+                        fontWeight: FontWeight.w600, // SemiBold
+                      ),
+                      items: dropdownItems.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(item),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        // 드롭다운 값 변경 로직
+                        if (newValue != null) {
+                          // 상태를 업데이트하는 로직 필요
+                          selectedValue = newValue;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                // 오른쪽 total 4 텍스트
+                Container(
+                  height: 24,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Center(
+                    child: RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'total ',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontFamily: 'AppleSDGothicNeo',
+                              fontWeight: FontWeight.w600, // SemiBold
+                              color: Colors.black,
+                            ),
+                          ),
+                          TextSpan(
+                            text: '4',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontFamily: 'AppleSDGothicNeo',
+                              fontWeight: FontWeight.w600, // SemiBold
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 파일 내용들 그리드
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 한 줄에 3개 아이템
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+                childAspectRatio: 93 / 101, // 파일 공간의 비율
+              ),
+              itemCount: 20, // 총 파일의 수
+              itemBuilder: (context, index) {
+                if (index == 19) { // 마지막 아이템에 점선 박스 추가
+                  return GestureDetector(
+                    onTap: () {
+                      // 생성박스 눌렀을 때 호출
+                      showModalBottomSheet(
+                        context: context,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(24),
+                          ),
+                        ),
+                        builder: (BuildContext context) {
+                          return Container(
+                            padding: EdgeInsets.all(16.0),
+                            height: 393,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(24),
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 28.0),
+                                  child: Image.asset(
+                                    'assets/images/logo.png', // 이미지 경로
+                                    height: 60,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+                                  child: Text(
+                                    '이번달 문제 만들기 1회 남았습니다',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: 'AppleSDGothicNeo',
+                                      fontWeight: FontWeight.bold, // Bold
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 38.0),
+                                  child: Text(
+                                    'pdf 업로드나 문제를 촬영하시면\n나의 문제집에 문제가 저장됩니다!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontFamily: 'AppleSDGothicNeo',
+                                      fontWeight: FontWeight.w500, // Medium
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      width: 174,
+                                      height: 60,
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          // PDF 업로드 로직
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Color(0xFF0B60B0), // 버튼 색상
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(15),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          'PDF 업로드',
+                                          style: TextStyle(color: Colors.white), // 글자 색상
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 6), // 버튼 사이의 패딩
+                                    SizedBox(
+                                      width: 174,
+                                      height: 60,
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          // 촬영하기 로직
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Color(0xFF0B60B0), // 버튼 색상
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(15),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          '촬영하기',
+                                          style: TextStyle(color: Colors.white), // 글자 색상
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: DottedBorder(
+                      color: Colors.grey,
+                      strokeWidth: 1,
+                      dashPattern: [4, 4],
+                      borderType: BorderType.RRect,
+                      radius: Radius.circular(16),
+                      child: Center(
+                        child: Text(
+                          '+',
+                          style: TextStyle(fontSize: 24.0, color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                return Container(
+                  decoration: BoxDecoration(
+                    color: ColorSystem.back, // 박스의 배경색 설정
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Image.asset(
+                          'assets/images/book_file.png', // 변경된 이미지 경로
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              '파일 이름 $index',
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                fontFamily: 'AppleSDGothicNeo',
+                                fontWeight: FontWeight.w600, // SemiBold
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(
+                              '생성 날짜: 2023-10-01',
+                              style: TextStyle(
+                                fontSize: 8.0,
+                                fontFamily: 'AppleSDGothicNeo',
+                                fontWeight: FontWeight.normal, // Regular
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),  
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          // 네브바 자리
+          // 네브바 구현 필요 시 여기에 추가
         ],
-      );
-    }
-    // );
+      ),
+    );
+  }
 }
