88f4a3988924146b38b4813163465256bd330ebd
JongHwa Paik <qorwhdghk12@gmail.com>
2025-04-07 22:44:32 +0900

    ✨ feat : design 업데이트

--- lib/viewmodel/book/book_view_model.dart
+++ lib/viewmodel/book/book_view_model.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:get/get.dart';
 import 'package:rest_test/utility/system/color_system.dart';
 
@@ -7,13 +6,45 @@ class BookViewModel extends GetxController {
   /* ------------------------------------------------------ */
   /* -------------------- DI Fields ----------------------- */
   /* ------------------------------------------------------ */
+  // Dependency Injection Fields, if needed
 
   /* ------------------------------------------------------ */
   /* ----------------- Private Fields --------------------- */
   /* ------------------------------------------------------ */
+  // Private fields for internal logic
 
   /* ------------------------------------------------------ */
   /* ----------------- Public Fields ---------------------- */
   /* ------------------------------------------------------ */
 
+  // 파일 목록 관리 (내용 제거)
+  var files = List.generate(20, (index) => {
+    'name': '파일 이름 $index',
+    'date': '생성 날짜: 2023-10-01'
+  }).obs;
+
+  // 총 파일 수
+  var total = 20.obs;
+
+  // 남은 문제 만들기 횟수
+  var remainingCount = 1.obs;
+
+  // 드롭다운 선택 값
+  var dropdownItems = ['최근 생성순', '가나다순', '최근 학습순'];
+  var selectedValue = '최근 생성순'.obs;
+
+  // 드롭다운 값 업데이트
+  void updateSelectedValue(String? newValue) {
+    if (newValue != null) {
+      selectedValue.value = newValue;
+    }
+  }
+
+  // 파일 목록에 파일 추가
+  void addFile(String name, String date) {
+    files.add({'name': name, 'date': date});
+    total.value = files.length;
+  }
+
+  // 추가적인 로직 및 상태 관리
 }
