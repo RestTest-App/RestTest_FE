b55083cc30af748540468d7d69a29985726b3e59
JH <jhk3261@gnu.ac.kr>
2025-06-19 01:37:21 +0900

    ✨ feat : 문항 연결

--- lib/model/home/exam_model.dart
+++ lib/model/home/exam_model.dart
@@ -17,7 +17,7 @@ class Exam {
     return Exam(
       examId: json['id'],
       examName: json['name'] ?? '',
-      questionCount: 0, // 실제 응답에 없음. 필요 시 서버에서 추가 or 임시 0
+      questionCount: json['question_count'], // 실제 응답에 없음. 필요 시 서버에서 추가 or 임시 0
       examTime: json['time'] ?? 0,
       passRate: (json['pass_rate'] as num?)?.toDouble() ?? 0.0,
     );
